29538677630a31d3e002a8376186818dce03fe03
NguyenVanHuy62TH1 <99159961+NguyenVanHuy62TH1@users.noreply.github.com>
2022-04-05 22:31:09 +0700

    Huy đã chỉnh sửa Huy_main.js

--- js/Huy_main.js
+++ js/Huy_main.js
@@ -1,12 +1,21 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop()) {
-            $('header').addClass('sticky');
-        }
-        else {
-            $('header').removeClass('sticky');
-        }
-    });
-});
-
-AOS.init();
+function send(){
+    //Kiểm tra thông tin được điền  
+    var arr = document.getElementsByTagName('input');
+    var name = arr[0].value;
+    var subject = arr[1].value;
+    if(name == "" || subject == ""){
+        alert("Vui lòng điền đầy đủ tên của bạn vào ông trống !");
+        return;
+    }
+    // Kiểm tra tính hợp lệ của email.
+    var email = document.getElementById('email'); 
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; 
+    if (!filter.test(email.value)) { 
+        alert('Hãy nhập địa chỉ email.\nExample@e.tlu.edu.vn');
+        email.focus; 
+        return false; 
+    }
+    else{ 
+        alert('Email của bạn hợp lệ.'); 
+    } 
+}
